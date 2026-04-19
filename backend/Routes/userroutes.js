6ef4b4bf9791696ca081baec610fac180cba660d
Anushka Tiwari <anushkatiwarii222@gmail.com>
2026-04-19 17:00:02 +0530

    Adding signup

--- backend/Routes/userroutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const {handleUserSignUp} = require('../Controllers/user');
-const router = express.Router();
-
-router.post('/' , handleUserSignUp);
-
-module.exports = router;
--- backend/newserver.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-
-const userRoute = require('./Routes/userroutes');
-
-app.use('/user' ,userRoute);
